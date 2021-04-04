3bad400d744a27faaf43750eb9af0e2e600a0a3b
Harrison Saliba <harrysaliba4139@gmail.com>
2021-04-03 22:57:35 -0700

    Added custom colors. Bootstrap navbar interfeares with colors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,30 @@ export class AppComponent {
 
   public resumeInfo: any = {};
 
+  public itemStyles: string = "";
+  public baseStyles: string = "";
+
   constructor(public resume: ResumeService, private titleService: Title) {
     resume.getResumeInfo().subscribe(data => {
       this.resumeInfo = data;
       titleService.setTitle(data.user.firstName + " " + data.user.lastName);
+      if (data.colors) {
+        // Set styles for body
+        if (data.colors.background) {
+          this.baseStyles += "background-color: " + data.colors.background + ";";
+        }
+        if (data.colors.foreground) {
+          this.baseStyles += "color: " + data.colors.foreground+ ";";
+        }
+        document.getElementById("color-base").setAttribute("style", this.baseStyles);
+        // Set styles for items
+        if (data.colors.itemForeground) {
+          this.itemStyles += "background-color: " + data.colors.itemBackground + ";";
+        }
+        if (data.colors.itemBackground) {
+          this.itemStyles += "color: " + data.colors.itemForeground+ ";";
+        }
+      }
     });
   }
 }
